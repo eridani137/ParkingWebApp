e68811e7a1126964a3a4177a56540bdd7f14e33c
Eridani 137 <tsuuzetsu@gmail.com>
2025-06-22 18:41:21 +0300

    ✨ feat(logging): Add Serilog logging configuration to Program.cs and reference required packages in Parking.WebApp.csproj

--- Parking.WebApp/Program.cs
+++ Parking.WebApp/Program.cs
@@ -2,8 +2,34 @@ using Microsoft.EntityFrameworkCore;
 using Parking.WebApp.Components;
 using Parking.WebApp.Data;
 using Parking.WebApp.Services;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+const string logs = "Logs";
+var logsPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, logs));
+
+if (!Directory.Exists(logsPath))
+{
+    Directory.CreateDirectory(logsPath);
+}
+
+const string outputTemplate =
+    "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+
+var levelSwitch = new LoggingLevelSwitch();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.ControlledBy(levelSwitch)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentName()
+    .WriteTo.Console(outputTemplate: outputTemplate, levelSwitch: levelSwitch)
+    .WriteTo.File($"{logsPath}/.log", rollingInterval: RollingInterval.Day, outputTemplate: outputTemplate, levelSwitch: levelSwitch)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSerilog();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
